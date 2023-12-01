6b1e0e80e29afbcb58575fdfc555e0b9a569121e
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2023-12-01 23:24:48 +1030

    feat(rust): :sparkles: solution for day 1 part 2

--- rust/solutions/day-01/src/lib.rs
+++ rust/solutions/day-01/src/lib.rs
@@ -1,3 +1,7 @@
+use itertools::FoldWhile::{Continue, Done};
+use itertools::Itertools;
+use std::collections::HashMap;
+
 pub fn process_part1(input: &str) -> String {
     input
         .lines()
@@ -24,33 +28,113 @@ pub fn process_part1(input: &str) -> String {
         .to_string()
 }
 
-pub fn process_part2(_input: &str) -> String {
-    "".to_string()
+pub fn extract_word_as_number(word: &str) -> Option<&str> {
+    let number_map = HashMap::from([
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+
+    for key in number_map.keys() {
+        if word.contains(key) {
+            return Some(number_map.get(key).unwrap());
+        };
+    }
+
+    None
+}
+
+pub fn get_first_number(line: &str) -> String {
+    line.chars()
+        .fold_while("".to_string(), |mut curr, char| {
+            if char.to_digit(10).is_some() {
+                return Done(char.to_string());
+            }
+
+            curr.push(char);
+            let extracted_number = extract_word_as_number(&curr);
+            if extracted_number.is_some() {
+                Done(extracted_number.unwrap().to_string())
+            } else {
+                Continue(curr)
+            }
+        })
+        .into_inner()
+}
+
+// Getting the last number for words we can't just iterate backwards
+// as the spelling of words will be reversed. There's definitely
+// a more elegant way to handle this!
+pub fn get_last_number(line: &str) -> String {
+    line.chars()
+        .rev()
+        .fold_while("".to_string(), |mut curr, char| {
+            if char.to_digit(10).is_some() {
+                return Done(char.to_string());
+            }
+
+            curr.insert(0, char);
+            let extracted_number = extract_word_as_number(&curr);
+            if extracted_number.is_some() {
+                Done(extracted_number.unwrap().to_string())
+            } else {
+                Continue(curr)
+            }
+        })
+        .into_inner()
+}
+
+pub fn process_part2(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let first_num = get_first_number(line);
+
+            let last_num = get_last_number(line);
+
+            String::from_iter([first_num, last_num])
+                .parse::<u32>()
+                .unwrap()
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "1abc2
+    #[test]
+    fn part_1_toy_input() {
+        const PART_1_INPUT: &str = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 ";
 
-    const PART_1_EXPECTED: &str = "142";
-    const PART_2_EXPECTED: &str = "";
-
-    #[test]
-    fn part_1_toy_input() {
-        let result = process_part1(INPUT);
+        const PART_1_EXPECTED: &str = "142";
+        let result = process_part1(PART_1_INPUT);
         assert_eq!(result, PART_1_EXPECTED);
     }
 
-    #[ignore] // Remove when doing part 2
     #[test]
     fn part_2_toy_input() {
-        let result = process_part2(INPUT);
+        const PART_2_INPUT: &str = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+        const PART_2_EXPECTED: &str = "281";
+        let result = process_part2(PART_2_INPUT);
         assert_eq!(result, PART_2_EXPECTED);
     }
 }
