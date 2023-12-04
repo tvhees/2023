d018070440b8c82f54ad23e5292185da5dc946ae
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2023-12-04 19:00:04 +1030

    refactor(rust): :recycle: extract function that isolates numbers from input line

--- rust/solutions/day-03/src/lib.rs
+++ rust/solutions/day-03/src/lib.rs
@@ -18,7 +18,71 @@ struct PossibleNumber {
     string: String,
 }
 
+fn get_possible_numbers_for_line(line: &str) -> Vec<PossibleNumber> {
+    let mut possible_numbers: Vec<PossibleNumber> = Vec::new();
+
+    line.chars()
+        .enumerate()
+        .fold(String::new(), |mut acc, item| {
+            let (i, character) = item;
+
+            if character.is_ascii_digit() {
+                acc.push(character);
+
+                if i == line.len() - 1 {
+                    possible_numbers.push(PossibleNumber {
+                        start: i - &acc.len(),
+                        end: i - 1,
+                        string: acc.clone(),
+                    });
+                }
+            } else if acc.len() > 0 {
+                possible_numbers.push(PossibleNumber {
+                    start: i - &acc.len(),
+                    end: i - 1,
+                    string: acc.clone(),
+                });
+                acc.clear();
+            }
+
+            return acc;
+        });
+
+    return possible_numbers;
+}
+
 pub fn process_part1(input: &str) -> String {
+    let len = input.lines().next().unwrap().len();
+    let empty_line = '.'.to_string().repeat(len);
+
+    iter::once(empty_line.as_str())
+        .chain(input.lines())
+        .chain(iter::once(empty_line.as_str()))
+        .tuple_windows::<(&str, &str, &str)>()
+        .flat_map(|(prev_line, current_line, next_line)| {
+            let possible_numbers = get_possible_numbers_for_line(current_line);
+
+            possible_numbers.into_iter().filter_map(|possible_number| {
+                let PossibleNumber { start, end, string } = possible_number;
+                let is_part_number = prev_line
+                    .chars()
+                    .enumerate()
+                    .chain(current_line.chars().enumerate())
+                    .chain(next_line.chars().enumerate())
+                    .any(|(i, character)| {
+                        character.is_symbol() && i >= 1.max(start) - 1 && i <= end + 1
+                    });
+
+                if is_part_number {
+                    Some(string.parse::<u32>().unwrap())
+                } else {
+                    None
+                }
+            })
+        })
+        .sum::<u32>()
+        .to_string()
+}
     let len = input.lines().next().unwrap().len();
     let empty_line = '.'.to_string().repeat(len);
 
