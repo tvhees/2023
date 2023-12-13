62d180b384a24c3d611cc02dc63f740f2ed46dfb
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2023-12-13 19:46:09 +1030

    feat(rust): :sparkles: solution for day 9 part 2

--- rust/solutions/day-09/src/lib.rs
+++ rust/solutions/day-09/src/lib.rs
@@ -34,18 +34,36 @@ pub fn process_part1(input: &str) -> String {
     pyramids.iter().map(get_next_value).sum::<i64>().to_string()
 }
 
-pub fn process_part2(_input: &str) -> String {
-    "placeholder".to_string()
+pub fn get_prev_value(input: &Vec<Vec<i64>>) -> i64 {
+    input
+        .iter()
+        .map(|row| *row.first().unwrap())
+        .rev()
+        .reduce(|acc, next| next - acc)
+        .unwrap()
+}
+
+pub fn process_part2(input: &str) -> String {
+    let histories = input.lines().map(|line| {
+        line.split_ascii_whitespace()
+            .map(|num| num.parse::<i64>().unwrap())
+            .collect_vec()
+    });
+
+    let pyramids = histories.map(|h| get_history_pyramid(h)).collect_vec();
+    pyramids.iter().map(get_prev_value).sum::<i64>().to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "0 1 1 0";
+    const INPUT: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
     const PART_1_EXPECTED: &str = "114";
-    const PART_2_EXPECTED: &str = "";
+    const PART_2_EXPECTED: &str = "2";
 
     #[test]
     fn part_1_toy_input() {
@@ -53,7 +71,6 @@ mod tests {
         assert_eq!(result, PART_1_EXPECTED);
     }
 
-    #[ignore] // Remove when doing part 2
     #[test]
     fn part_2_toy_input() {
         let result = process_part2(INPUT);
