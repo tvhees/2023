5e746e54f15aece4eefa0ce2e3106d0c31eb44b7
Torbjorn van Heeswijck <torbjorn.vanheeswijck@aligent.com.au>
2023-12-13 22:28:15 +1030

    feat(rust): :sparkles: solution for day 10 part 2

--- rust/solutions/day-10/src/lib.rs
+++ rust/solutions/day-10/src/lib.rs
@@ -1,5 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
+use itertools::Itertools;
+
+#[derive(Debug)]
 pub enum Direction {
     Up,
     Down,
@@ -44,10 +47,10 @@ pub fn path_length(
     position: (usize, usize),
     map: &HashMap<(usize, usize), char>,
     direction: Direction,
-    count: usize,
-) -> Option<usize> {
+    mut tiles: HashSet<(usize, usize)>,
+) -> Option<HashSet<(usize, usize)>> {
     if matches!(direction, Direction::Start) {
-        return Some(count);
+        return Some(tiles);
     } else if matches!(direction, Direction::None) {
         return None;
     }
@@ -92,7 +95,8 @@ pub fn path_length(
         _ => Direction::None,
     };
 
-    path_length(next_position.unwrap(), map, next_direction, count + 1)
+    tiles.insert(position);
+    path_length(next_position.unwrap(), map, next_direction, tiles)
 }
 
 pub fn process_part1(input: &str) -> String {
@@ -114,8 +118,8 @@ pub fn process_part1(input: &str) -> String {
         Direction::Left,
         Direction::Right,
     ] {
-        if let Some(length) = path_length(position, &map, direction, 0) {
-            steps = length / 2;
+        if let Some(tiles) = path_length(position, &map, direction, HashSet::new()) {
+            steps = tiles.len() / 2;
             break;
         }
     }
@@ -123,32 +127,87 @@ pub fn process_part1(input: &str) -> String {
     steps.to_string()
 }
 
-pub fn process_part2(_input: &str) -> String {
-    "placeholder".to_string()
+pub fn process_part2(input: &str) -> String {
+    // Collect travelled tiles in hashset
+    // Then for each line in grid, check how many vertical downward (|, L, J) tiles are crossed
+    // to get to it. If Odd number > 0, it's inside the path
+    let grid = input.lines().enumerate().flat_map(|(i, line)| {
+        line.chars()
+            .enumerate()
+            .map(move |(j, char)| ((i, j), char))
+    });
+
+    let position = grid.clone().find(|entry| entry.1 == 'S').unwrap().0;
+
+    let map: HashMap<(usize, usize), char> = HashMap::from_iter(grid);
+
+    let path_positions = [
+        Direction::Up,
+        Direction::Down,
+        Direction::Left,
+        Direction::Right,
+    ]
+    .into_iter()
+    .filter_map(|direction| path_length(position, &map, direction, HashSet::new()))
+    .next()
+    .unwrap();
+
+    let rows = input.lines().collect_vec().len();
+    let cols = map.len() / rows;
+
+    let mut enclosed: usize = 0;
+
+    for i in 0..rows {
+        let mut pipes_crossed = 0;
+        for j in 0..cols {
+            if path_positions.contains(&(i, j)) {
+                match map.get(&(i, j)).unwrap() {
+                    // Technically I should handle whether S is a vertical pipe or not!
+                    '|' | 'L' | 'J' | 'S' => pipes_crossed += 1,
+                    _ => (),
+                };
+            } else if pipes_crossed % 2 > 0 {
+                enclosed += 1;
+            }
+        }
+    }
+
+    enclosed.to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "7-F7-
+    #[test]
+    fn part_1_toy_input() {
+        const INPUT: &str = "7-F7-
 .FJ|7
 SJLL7
 |F--J
 LJ.LJ";
 
-    const PART_1_EXPECTED: &str = "8";
-    const PART_2_EXPECTED: &str = "";
+        const PART_1_EXPECTED: &str = "8";
 
-    #[test]
-    fn part_1_toy_input() {
         let result = process_part1(INPUT);
         assert_eq!(result, PART_1_EXPECTED);
     }
 
-    #[ignore] // Remove when doing part 2
     #[test]
     fn part_2_toy_input() {
+        const INPUT: &str = "FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L";
+
+        const PART_2_EXPECTED: &str = "10";
+
         let result = process_part2(INPUT);
         assert_eq!(result, PART_2_EXPECTED);
     }
